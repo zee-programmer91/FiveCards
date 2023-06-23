ec9414c20ce41f1b748aab409dc6edfcd0582f0a
Zwelisha Langa <zwelisha@bbd.co.za>
2023-06-23 16:14:20 +0200

    Added logic to display the game interface Added these new file: - Board.txt - CardPosition.h

--- Board.h
+++ Board.h
@@ -1,5 +1,18 @@
 #pragma once
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
 class Board
 {
+	public:
+		Board(std::string filePath);
+		~Board();
+		void displayBoard();
+	private:
+		void readFromFile();
+		std::vector<std::string> boardInterface{};
+		std::ifstream file{};
 };
-
---
+++ CardPositions.h
@@ -0,0 +1,25 @@
+#pragma once
+
+enum Cards
+{
+	card1 = 3,
+	card1 = 8,
+	card1 = 13,
+	card1 = 18,
+	card1 = 23,
+};
+enum Computer
+{
+	row = 1,
+};
+
+enum Garbage
+{
+	row = 5,
+	position = 10,
+};
+
+enum Player
+{
+	row = 9,
+};
