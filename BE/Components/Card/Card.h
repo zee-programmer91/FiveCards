dcd676e340613cfcd587f6d0b260c8025e64ef9d
Zwelisha Brian Langa <81659005+zee-programmer91@users.noreply.github.com>
2023-10-30 16:20:11 +0200

    Develop -> Master (#50)
    * Remove Card DLL (#49)
    
    - Changed Card Component From Being A DLL
    
    Co-authored-by: Zwelisha Langa <zwelisha@bbd.co.za>

--- BE/Components/Card/Card.h
+++ BE/Components/Card/Card.h
@@ -2,41 +2,15 @@
 #include "CardEnums.h"
 #include <string>
 
-#ifdef CARD_EXPORTS
-#define CARD_API __declspec(dllexport)
-#else
-#define CARD_API __declspec(dllimport)
-#endif
-
-struct Card
+class Card
 {
-	std::string cardValue;
-	CardTypes cardType;
-
-	std::string getCardInfor()
-	{
-		return cardValue + getCardType();
-	};
-
-	std::string getCardValue()
-	{
-		return cardValue;
-	};
-
-	std::string getCardType()
-	{
-		switch (cardType)
-		{
-		case CardTypes::Clubs:
-			return "C";
-		case CardTypes::Diamonds:
-			return "D";
-		case CardTypes::Hearts:
-			return "H";
-		case CardTypes::Spades:
-			return "S";
-		default:
-			return "X";
-		}
-	};
+public:
+	Card();
+	Card(std::string value, CardTypes type);
+	std::string getCardInfor();
+	std::string getCardValue();
+	std::string getCardType();
+protected:
+	std::string value;
+	CardTypes type;
 };
