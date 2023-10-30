29163aeb5f40fd6c293da47cb3ed208fb553b127
Zwelisha Brian Langa <81659005+zee-programmer91@users.noreply.github.com>
2023-10-30 16:04:35 +0200

    Remove Card DLL (#49)
    - Changed Card Component From Being A DLL
    
    Co-authored-by: Zwelisha Langa <zwelisha@bbd.co.za>

--- BE/Components/Card/Card.cpp
+++ BE/Components/Card/Card.cpp
@@ -1,3 +1,36 @@
 #include "Card.h"
-#include "pch.h"
 #include <string>
+
+Card::Card()
+{
+	
+};
+
+Card::Card(std::string cardValue, CardTypes cardType): value(cardValue), type(cardType) {};
+
+std::string Card::getCardInfor()
+{
+	return value + getCardType();
+};
+
+std::string Card::getCardValue()
+{
+	return value;
+};
+
+std::string Card::getCardType()
+{
+	switch (type)
+	{
+	case CardTypes::Clubs:
+		return "C";
+	case CardTypes::Diamonds:
+		return "D";
+	case CardTypes::Hearts:
+		return "H";
+	case CardTypes::Spades:
+		return "S";
+	default:
+		return "X";
+	}
+};
