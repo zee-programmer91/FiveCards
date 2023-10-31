7b8fe7074e87cc3d999c89a930ec1586c4281c7b
Zwelisha Langa <zwelisha@bbd.co.za>
2023-10-31 22:41:35 +0200

    CreateWebAPI - I can now get all players

--- FiveCardsWeb/DBConnections/MongoDBConnection.cs
+++ FiveCardsWeb/DBConnections/MongoDBConnection.cs
@@ -11,16 +11,23 @@ namespace FiveCardsWeb
     class MongoDBConnection
     {
         private readonly MongoClient dbConnection;
+        private readonly IMongoDatabase fiveCardsDatabase;
         private readonly string databaseName = "FiveCards";
 
         public MongoDBConnection(string connectionStr)
         {
             dbConnection = new MongoClient(connectionStr);
+            fiveCardsDatabase = dbConnection.GetDatabase(databaseName);
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            var filter = Builders<Player>.Filter.Empty;
+            return fiveCardsDatabase.GetCollection<Player>("Player").Find<Player>(filter).ToList();
         }
 
         public Player GetPlayerById(string idString)
         {
-            var fiveCardsDatabase = dbConnection.GetDatabase(databaseName);
             var filter = Builders<Player>.Filter.Eq(player => player._id, new ObjectId(idString));
             return fiveCardsDatabase.GetCollection<Player>("Player").Find<Player>(filter).FirstOrDefault();
         }
