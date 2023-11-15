b28f0a82506c13c3420ddad8933190f1a42ca85c
Zwelisha Brian Langa <81659005+zee-programmer91@users.noreply.github.com>
2023-11-15 14:32:07 +0200

    PlayerCardsAPI (#53)
    - Added PlayerCards Model
    - Added SQL Scripts
    
    Co-authored-by: Zwelisha Langa <zwelisha@bbd.co.za>

---
+++ Web/FiveCardsAPI/Models/PlayerCards.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiveCardsAPI.Models
+{
+    public class PlayerCards
+    {
+        public int PlayerCarsID { get; set; }
+        public int Card1ID { get; set; }
+        public int Card2ID { get; set; }
+        public int Card3ID { get; set; }
+        public int Card4ID { get; set; }
+        public int Card5ID { get; set; }
+    }
+}
