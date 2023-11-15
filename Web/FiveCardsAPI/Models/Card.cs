7a8afbc0fbeb06ffa201eab58856c8d7f81daf9c
Zwelisha Brian Langa <81659005+zee-programmer91@users.noreply.github.com>
2023-11-15 14:54:17 +0200

    Models (#54)
    - Added Garbage Model
    - Added DeckCards Model
    - Added Deck Model
    - Added CardType Model
    - Added Card Model
    - Added BoardPlayers Model
    - Added Board Model
    
    Co-authored-by: Zwelisha Langa <zwelisha@bbd.co.za>

---
+++ Web/FiveCardsAPI/Models/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiveCardsAPI.Models
+{
+    public class Card
+    {
+        public int CardID { get; set; }
+        public int CardValue { get; set; }
+        public int CardTypeID { get; set; }
+    }
+}
---
+++ Web/FiveCardsAPI/Models/CardType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiveCardsAPI.Models
+{
+    public class CardType
+    {
+        public int CardTypeID { get; set; }
+        public string Type { get; set; }
+
+    }
+}
